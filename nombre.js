e609f5a08ba658a6ae02a01231c7466126478f87
Sarah CHERCHEM <ls_cherchem@esi.dz>
2024-02-10 00:06:31 +0100

    Ajout de la fonction start_player_guess_game

--- nombre.js
+++ nombre.js
@@ -17,6 +17,35 @@ function new_game() { // fonction qui montre l’´ecran de s´election du mode 
     show(game_selection);
     hide(game1);
     hide(game2);
-    }
+}
+
+function start_player_guess_game(){
+    computer_number=generateRandomNumber(1,100);
+
+
+    show(game1);
+    hide(game_selection);
+
+    hide(restart_button1);
+
+    try_button.button.addEventListener("click",function(){
+        let player_guess=parseInt(player_input.value);
+
+
+        if(player_guess<computer_number)
+        {
+            message1.innerText='Choisissez un nombre plus grand';  
+        }else if(player_guess> computer_number) {
+            message1.innerText="Choisissez un nombre plus petit";
+        }else{
+            message1.innerText="Bravo, vous l'avez trouvé";
+             show(restart_button1);
+        }
+      
+        player_input.value="";
+        
+    });
+    restart_button1.addEventListener("click",new_game);
+}
 
 
