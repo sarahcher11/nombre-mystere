f6275de5e7701c130d050eb0d1bf36a446e3cb11
Sarah CHERCHEM <ls_cherchem@esi.dz>
2024-02-09 23:54:45 +0100

    Ajout de la fonction show

--- nombre.js
+++ nombre.js
@@ -1,3 +1,13 @@
 let message1=document.querySelector('#message1');
 
+message1.innerText="Un petit text";
+
+console.log(input_element.value);
+
+
+function show(element){
+    element.style.display='';
+}
+
+
 
