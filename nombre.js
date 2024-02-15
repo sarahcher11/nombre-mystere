b031fdd9ce0ca81b0dd28f65bf6e9ed8c071afe9
Sarah CHERCHEM <ls_cherchem@esi.dz>
2024-02-15 14:00:50 +0100

    Ajout des fonctionnalités

--- nombre.js
+++ nombre.js
@@ -1,51 +1,52 @@
-let message1=document.querySelector('#message1');
+let message1 = document.getElementById('message1');
+let restart_button1 = document.getElementById("restart_button1");
+let game1 = document.getElementById("game1");
+let game2 = document.getElementById('game2');
+let try_button = document.getElementById("try_button");
+let game_selection=document.getElementById('game_selection');
+message1.innerText = "Un petit texte";
 
-message1.innerText="Un petit text";
+console.log(player_input.value); // Utilisez player_input au lieu de input_element
 
-console.log(input_element.value);
-
-
-function show(element){
-    element.style.display='';
+function show(element) {
+    element.style.display = '';
 }
 
-function hide(element){
-    element.style.display='none';
+function hide(element) {
+    element.style.display = 'none';
 }
 
-function new_game() { // fonction qui montre l’´ecran de s´election du mode de jeu
+function new_game() { 
     show(game_selection);
     hide(game1);
     hide(game2);
 }
 
-function start_player_guess_game(){
-    computer_number=generateRandomNumber(1,100);
-
+function start_player_guess_game() {
+    let computer_number = Math.floor(Math.random() * 100) + 1;
 
     show(game1);
     hide(game_selection);
-
     hide(restart_button1);
 
-    try_button.button.addEventListener("click",function(){
-        let player_guess=parseInt(player_input.value);
+    try_button.addEventListener("click", function() {
+        let player_input = document.getElementById("player_input");
+        let player_guess = parseInt(player_input.value);
 
-
-        if(player_guess<computer_number)
-        {
-            message1.innerText='Choisissez un nombre plus grand';  
-        }else if(player_guess> computer_number) {
-            message1.innerText="Choisissez un nombre plus petit";
-        }else{
-            message1.innerText="Bravo, vous l'avez trouvé";
-             show(restart_button1);
+        if (player_guess < computer_number) {
+            message1.innerText = 'Choisissez un nombre plus grand';  
+        } else if (player_guess > computer_number) {
+            message1.innerText = "Choisissez un nombre plus petit";
+        } else {
+            message1.innerText = "Bravo, vous l'avez trouvé";
+            show(restart_button1);
         }
       
-        player_input.value="";
-        
+        player_input.value = "";
     });
-    restart_button1.addEventListener("click",new_game);
+    
+    restart_button1.addEventListener("click", new_game);
 }
 
-
+let start_game1 = document.getElementById('start_game1');
+start_game1.addEventListener("click", start_player_guess_game);
